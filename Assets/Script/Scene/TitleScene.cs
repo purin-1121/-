7c3145caf1f9c8c797d6dc020b38ff9524a70eae
it252070-sk <it252070@trident.ac.jp>
2026-05-12 18:59:34 +0900

    Update Title
    タイトルスクリプトに音とボタンでシーンを移動するコードを追加

--- Assets/Script/Scene/TitleScene.cs
+++ Assets/Script/Scene/TitleScene.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleScene : MonoBehaviour
 {
+    public AudioClip clickSE;
+    AudioSource audioSource;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void GoToGamePlay()
     {
-        
+        SceneManager.LoadScene("GamePlayScene");
+    }
+
+    public void OnStartButton()
+    {
+        //‰¹‚ğ–Â‚ç‚·
+        if (audioSource != null && clickSE != null)
+        {
+            audioSource.PlayOneShot(clickSE);
+        }
+
+        // ‚·‚®‚ÉƒV[ƒ“‚ª•Ï‚í‚é‚Æ‰¹‚ª“rØ‚ê‚é‚Ì‚ÅA­‚µ‚¾‚¯’x‚ç‚¹‚ÄˆÚ“®
+        Invoke("GoToGamePlay", 0.5f);
     }
 }
